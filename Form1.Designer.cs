e2d638a0d8b743613f409e969a8a29c2398005db
Murilorasg <murilo.rasg@gmail.com>
2021-02-15 16:40:54 -0300

    Commit inicial da Calculadora de IMC

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace IMC_Calculator
+{
+    partial class formIMCCalc
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblIMC = new System.Windows.Forms.Label();
+            this.lblSitu = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtIMC = new System.Windows.Forms.TextBox();
+            this.txtSituacao = new System.Windows.Forms.TextBox();
+            this.btnSubm = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(164, 24);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(34, 13);
+            this.lblAltura.TabIndex = 0;
+            this.lblAltura.Text = "Altura";
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Location = new System.Drawing.Point(165, 77);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(31, 13);
+            this.lblPeso.TabIndex = 1;
+            this.lblPeso.Text = "Peso";
+            // 
+            // lblIMC
+            // 
+            this.lblIMC.AutoSize = true;
+            this.lblIMC.Location = new System.Drawing.Point(167, 133);
+            this.lblIMC.Name = "lblIMC";
+            this.lblIMC.Size = new System.Drawing.Size(26, 13);
+            this.lblIMC.TabIndex = 2;
+            this.lblIMC.Text = "IMC";
+            // 
+            // lblSitu
+            // 
+            this.lblSitu.AutoSize = true;
+            this.lblSitu.Location = new System.Drawing.Point(157, 185);
+            this.lblSitu.Name = "lblSitu";
+            this.lblSitu.Size = new System.Drawing.Size(49, 13);
+            this.lblSitu.TabIndex = 3;
+            this.lblSitu.Text = "Situação";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(130, 46);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 4;
+            this.txtAltura.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(130, 101);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 5;
+            this.txtPeso.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtIMC
+            // 
+            this.txtIMC.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtIMC.Enabled = false;
+            this.txtIMC.Location = new System.Drawing.Point(130, 156);
+            this.txtIMC.Name = "txtIMC";
+            this.txtIMC.Size = new System.Drawing.Size(100, 20);
+            this.txtIMC.TabIndex = 6;
+            this.txtIMC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtSituacao
+            // 
+            this.txtSituacao.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtSituacao.Enabled = false;
+            this.txtSituacao.Location = new System.Drawing.Point(24, 210);
+            this.txtSituacao.Name = "txtSituacao";
+            this.txtSituacao.Size = new System.Drawing.Size(324, 20);
+            this.txtSituacao.TabIndex = 7;
+            this.txtSituacao.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnSubm
+            // 
+            this.btnSubm.Location = new System.Drawing.Point(24, 256);
+            this.btnSubm.Name = "btnSubm";
+            this.btnSubm.Size = new System.Drawing.Size(82, 30);
+            this.btnSubm.TabIndex = 8;
+            this.btnSubm.Text = "Submeter";
+            this.btnSubm.UseVisualStyleBackColor = true;
+            this.btnSubm.Click += new System.EventHandler(this.btnSubm_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(143, 256);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(82, 30);
+            this.btnLimpar.TabIndex = 9;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(266, 256);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(82, 30);
+            this.btnSair.TabIndex = 10;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // formIMCCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 319);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSubm);
+            this.Controls.Add(this.txtSituacao);
+            this.Controls.Add(this.txtIMC);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.lblSitu);
+            this.Controls.Add(this.lblIMC);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.lblAltura);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "formIMCCalc";
+            this.Text = "Calculadora de IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblIMC;
+        private System.Windows.Forms.Label lblSitu;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtIMC;
+        private System.Windows.Forms.TextBox txtSituacao;
+        private System.Windows.Forms.Button btnSubm;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMC_Calculator
+{
+    public partial class formIMCCalc : Form
+    {
+        public formIMCCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubm_Click(object sender, EventArgs e)
+        {
+            double altura, peso, imc;
+
+            altura = Convert.ToDouble(txtAltura.Text);
+            peso = Convert.ToDouble(txtPeso.Text);
+
+            imc = peso / (altura * altura);
+
+            txtIMC.Text = Convert.ToString(imc.ToString("F"));
+
+            if (imc < 17)
+            {
+                txtSituacao.Text = "Você está muito abaixo do peso";
+            }
+            else if (imc >= 17 && imc < 18.49)
+            {
+                txtSituacao.Text = "Você está abaixo do peso";
+            }
+            else if (imc >= 18.50 && imc < 24.99)
+            {
+                txtSituacao.Text = "Parabéns! Você está em seu peso ideal!";
+            }
+            else if (imc >= 25 && imc < 29.99)
+            {
+                txtSituacao.Text = "Você está acima do peso";
+            }
+            else if (imc >= 30 && imc < 34.99)
+            {
+                txtSituacao.Text = "Atenção! Você está com obesidade classe I";
+            }
+            else if (imc >= 35 && imc < 39.99)
+            {
+                txtSituacao.Text = "Atenção! Você está com obesidade classe II (severa)";
+            }
+            else
+            {
+                txtSituacao.Text = "Muita atenção!!! Você está com obesidade classe III (mórbida)";
+            }
+
+            txtSituacao.Enabled = true;
+            txtIMC.Enabled = true;
+            
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtAltura.Clear();
+            txtPeso.Clear();
+            txtIMC.Clear();
+            txtSituacao.Clear();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
